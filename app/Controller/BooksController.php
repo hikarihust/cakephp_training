e61ac8ba0060375c13daa825b61cef72287bfd11
quang <v-quang@ostechnology.co.jp>
2018-12-12 19:09:57 +0900

    Cac phuong thuc truy van du lieu

--- app/Controller/BooksController.php
+++ app/Controller/BooksController.php
@@ -6,5 +6,17 @@ App::uses('AppController', 'Controller');
  * 
  */
 class BooksController extends AppController{
+	public function truyvan(){
+		$books = $this->Book->find('all', array(
+			'recursive' => -1,
+			'fields' => array('id', 'title'),
+			'conditions' => array('id <' => 11),
+			'order' => array('title' => 'asc'),
+			'limit' => 5
+		));
 
+		// $books = $this->Book->query("select id, title from books");
+		pr($books);
+		die();
+	}
 }
