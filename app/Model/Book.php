47b59f14916ef6e61049ce0552f28f1d46e777e5
quang <v-quang@ostechnology.co.jp>
2018-12-12 16:33:18 +0900

    Tao Model va Controller cho Book, co so du lieu cho website

---
+++ app/Model/Book.php
@@ -0,0 +1,47 @@
+<?php 
+
+App::uses('AppModel', 'Model');
+class Book extends AppModel{
+	public $useTable = 'books';
+	public $validate = array(
+	);
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Category' => array(
+			'className' => 'Category',
+			'foreignKey' => 'category_id',
+		)
+	);
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Comment' => array(
+			'className' => 'Comment',
+			'foreignKey' => 'book_id',
+		)
+	);
+
+
+/**
+ * hasAndBelongsToMany associations
+ *
+ * @var array
+ */
+	public $hasAndBelongsToMany = array(
+		'Writer' => array(
+			'className' => 'Writer',
+			'joinTable' => 'books_writers',
+			'foreignKey' => 'book_id',
+			'associationForeignKey' => 'writer_id',
+		)
+	);
+}
