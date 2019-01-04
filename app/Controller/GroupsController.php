1fc7affb79411c789c6fb815b9b4e3e4514fb796
quang <v-quang@ostechnology.co.jp>
2019-01-04 15:09:40 +0900

    phan quyen cho cac nhom nguoi dung admin, manager, user, banned

--- app/Controller/GroupsController.php
+++ app/Controller/GroupsController.php
@@ -36,8 +36,65 @@ class GroupsController extends AppController {
 	// 	exit;
 	// }
 
+/**
+ * Thiết lập quyền cho user bình thường
+ * ở đây chỉ cần cấp quyền cho user sử dụng những action yêu cầu đăng nhập để thực hiện
+ * những action không yêu cầu đăng nhập, đã được Auth Component allow thì không cần cấp quyền ở đây
+ */
+	private function user_roles($model = 'Group', $foreign_key = null){
+		//quyền gởi comment
+		$this->Acl->allow(array('model'=>$model, 'foreign_key'=>$foreign_key),'Comments/add');
+		
+		//quyền xem thông tin đơn hàng, check out khi thanh toán
+		$this->Acl->allow(array('model'=>$model, 'foreign_key'=>$foreign_key),'Orders/history');
+		$this->Acl->allow(array('model'=>$model, 'foreign_key'=>$foreign_key),'Orders/detail');
+		$this->Acl->allow(array('model'=>$model, 'foreign_key'=>$foreign_key),'Orders/checkout');
+
+		//quyền cập nhật thông tin/password, logout
+		$this->Acl->allow(array('model'=>$model, 'foreign_key'=>$foreign_key),'Users/logout');
+		$this->Acl->allow(array('model'=>$model, 'foreign_key'=>$foreign_key),'Users/change_password');
+		$this->Acl->allow(array('model'=>$model, 'foreign_key'=>$foreign_key),'Users/change_info');
+	}
+/**
+ * Thiết lập quyền quản lý cho manager
+ */
+	private function manager_roles($model = 'Group', $foreign_key = null){
+		//manager cũng có quyền của user bình thường
+		$this->user_roles($model,$foreign_key);
+
+		//quyền quản lý books, categories và writers
+		$this->Acl->allow(array('model'=>$model, 'foreign_key'=>$foreign_key),'Books');
+		$this->Acl->allow(array('model'=>$model, 'foreign_key'=>$foreign_key),'Categories');
+		$this->Acl->allow(array('model'=>$model, 'foreign_key'=>$foreign_key),'Writers');
+
+		//không cho phép xóa books, categories và writers
+		$this->Acl->deny(array('model'=>$model, 'foreign_key'=>$foreign_key),'Books/admin_delete');
+		$this->Acl->deny(array('model'=>$model, 'foreign_key'=>$foreign_key),'Categories/admin_delete');
+		$this->Acl->deny(array('model'=>$model, 'foreign_key'=>$foreign_key),'Writers/admin_delete');
+	}
+
+/**
+ * Hàm thiết lập quyền người dùng, gọi hàm này trên trình duyệt và chỉ chạy 1 lần
+ * do hàm này mức truy cập là public nên phải update vào bảng acos mới chạy được.
+ * gọi xong hàm này thì disable nó đi :)
+ * trong hàm install hiện tại chỉ thiết lập quyền cho 1 người dùng là admin và 3 nhóm ng dùng là manager, user, và banned, 
+ */
 	// public function admin_install(){
-	// 	$this->Acl->allow(array('model'=>'Group', 'foreign_key'=>5),'Users/logout');
+	// 	//set quyền cho user admin có toàn quyền, user_id = 1
+	// 	$this->Acl->allow(array('model'=>'User', 'foreign_key'=>1),'controllers');
+
+	// 	//set quyền cho user bình thường, có các quyền cơ bản, group_id = 2 - group user
+	// 	$this->user_roles('Group', 2);
+
+	// 	//set quyền cho group quản lý, ngoài quyền cơ bản còn có thêm các quyền quản lý (books, categories và writers)
+	// 	// group_id = 3 - group manager
+	// 	$this->manager_roles('Group', 5);
+
+	// 	//set quyền cho group bị cấm, không có bất kì quyền gì trên ChickenRainShop
+	// 	// group_id = 4 - group banned
+	// 	$this->Acl->deny(array('model'=>'Group', 'foreign_key'=>3),'controllers');
+
+	// 	exit; //gọi lệnh này để không bị lỗi missing view
 	// }
 
 /**
